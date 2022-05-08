8df25eeb96693f95f1b5cf96eeadceb645f339d3
ggongdori <soominlee0410@gmail.com>
2022-05-08 20:34:27 +0900

    dto added

---
+++ src/main/java/com/example/socket/ChatMessageDto.java
@@ -0,0 +1,13 @@
+package com.example.socket;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessageDto {
+
+    private String roomId;
+    private String writer;
+    private String message;
+}
