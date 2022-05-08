51ab8aa6a7b0924227c3694a042989ee3f632349
ggongdori <soominlee0410@gmail.com>
2022-05-08 20:28:54 +0900

    websocket modified

---
+++ src/main/java/com/example/socket/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.socket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Slf4j
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().sameOrigin();
+    }
+}
